30f3cc229f6476686c2023c88c1d182c82e7d9cd
Shaun Knowles <shaun@primitivedigital.uk>
2023-01-04 11:30:30 +0000

    format

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@
  */
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes, Outlet, Link} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet, Link } from "react-router-dom";
 
 import Header from "./header";
 import Footer from "./footer";
@@ -25,7 +25,6 @@ ReactGA.initialize("UA-7143300-34");
 require("./style.scss");
 
 function App() {
-
   // console.log(process.env.NODE_ENV);
 
   return (
@@ -35,22 +34,21 @@ function App() {
             parent route elements. */}
       <Routes>
         <Route path={PrimitiveSettings.path} element={<Layout />}>
-
           <Route index element={<Posts />} />
-          <Route path="posts/:slug" element={<Post />}/>
-          <Route path={"category/"} element={<Categories />}/>
-          <Route path={"category/:slug"} element={<Categories />}/>
-          <Route path={"tag/"} element={<Tags />}/>
-          <Route path={"tag/:slug"} element={<Tags />}/>
+          <Route path="posts/:slug" element={<Post />} />
+          <Route path={"category/"} element={<Categories />} />
+          <Route path={"category/:slug"} element={<Categories />} />
+          <Route path={"tag/"} element={<Tags />} />
+          <Route path={"tag/:slug"} element={<Tags />} />
 
-          <Route path={"jokes/"} element={<Jokes />}/>
-          <Route path={"jokes/:slug"} element={<Joke />}/>
-          <Route path={"jokes/about/:slug"} element={<JokeTags />}/>
-          <Route path={"jokes/by-type/:slug"} element={<JokeCats />}/>
+          <Route path={"jokes/"} element={<Jokes />} />
+          <Route path={"jokes/:slug"} element={<Joke />} />
+          <Route path={"jokes/about/:slug"} element={<JokeTags />} />
+          <Route path={"jokes/by-type/:slug"} element={<JokeCats />} />
 
-          <Route path={"think/:slug"} element={Page}/>
-          <Route path={"life/:slug"} element={Page}/>
-          <Route path={":slug"} element={Page}/>
+          <Route path={"think/:slug"} element={<Page />} />
+          <Route path={"life/:slug"} element={<Page />} />
+          <Route path={":slug"} element={<Page />} />
 
           {/* Using path="*"" means "match anything", so this route acts like a catch-all for URLs 
             that we don't have explicit routes for. */}
@@ -74,16 +72,15 @@ function Layout() {
 }
 
 function NoMatch() {
-    return (
-      <div>
-        <h2>Nothing to see here!</h2>
-        <p>
-          <Link to="/">Go to the home page</Link>
-        </p>
-      </div>
-    );
-  }
-
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page </Link>
+      </p>
+    </div>
+  );
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
