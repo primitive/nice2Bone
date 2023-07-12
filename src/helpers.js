040f05245ffcba01c13fd51558caa72c8ed2d0c6
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-12 13:37:48 +0100

    extract helper fn

--- src/helpers.js
+++ src/helpers.js
@@ -1,18 +1,20 @@
-
-
-export function isEmpty(obj) {
-    for(var key in obj) {
-        if(obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
+export const isEmpty = (obj) => {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
   }
+  return true;
+};
 
-  /*
+export const handleBeforeUnload = () => {
+  window.scrollTo(0, 0);
+  console.log("before unload");
+};
+
+/*
   export function convertUnicode(input) {
     return input.replace(/\\u(\w\w\w\w)/g,function(a,b) {
       var charcode = parseInt(b,16);
       return String.fromCharCode(charcode);
     });
   }
-  */
+*/
