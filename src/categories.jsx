3d0f8c2ab198e3eaf90b391e67bbe071d8c72141
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-13 17:49:30 +0100

    update

--- src/categories.jsx
+++ src/categories.jsx
@@ -1,97 +1,51 @@
-// External dependencies
-import React from "react";
-import withRouter from './withrouter';
-import PostList from "./post-list";
-import LoadingIcon from "./loading-icon.gif";
-import ReactGA from 'react-ga';
+/**
+ * The Categories Component
+ * @package Nice2B One
+ * 2023
+ */
+import React, { useState, useEffect } from "react";
+import { Rings as Loader } from "react-loader-spinner";
+import PostList from "./rocks/post-list";
+import { handleBeforeUnload } from "./helpers";
+// import ReactGA from "react-ga";
 
-class Categories extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      category: "",
-      page: 0,
-      getPostsInCat: true,
-      controller: false
-    };
-    this.getMorePostsInCat = this.getMorePostsInCat.bind(this);
-  }
+const Categories = (props) => {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [category, setCategory] = useState("");
+  const [pageNo, setPageNo] = useState(1);
+  const [getPostsInCat, setGetPostsInCat] = useState(true);
 
-  componentWillUnmount() {
-    this.getMorePostsInCat = null;
-  }
-
-  componentDidMount() {
-    window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
-    };
+  useEffect(() => {
 
     // init ScrollMagic Controller
-    this.controller = new ScrollMagic.Controller();
-
-    // build scene
-    const scene = new ScrollMagic.Scene({
+    const controller = new ScrollMagic.Controller();
+    //setController(new ScrollMagic.Controller());
+    new ScrollMagic.Scene({
       triggerElement: "#footer",
-      triggerHook: "onEnter"
+      triggerHook: "onEnter",
     })
-      .addTo(this.controller)
-      .on("enter", e => {
-        if (this.state.getPostsInCat && this.getMorePostsInCat !== null) {
-          this.getMorePostsInCat();
+      .addTo(controller)
+      .on("enter", (e) => {
+        if (getPostsInCat) {
+          getMorePostsInCat();
         }
       });
 
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    document.title = PrimitiveSettings.theme_name + " - " + PrimitiveSettings.theme_posts_title;
     document.body.className = "";
-    document.body.classList.add('category-list');
-  }
+    document.body.classList.add("category-list");
 
-  getMorePostsInCat() {
-    let url = window.location.href.split("/");
-    let slug = url.pop() || url.pop();
-    let totalPages;
+    //ReactGA.pageview(window.location.pathname + window.location.search);
 
-    this.setState({
-      page: this.state.page + 1,
-      category: slug
-    });
+    window.onbeforeunload = handleBeforeUnload;
 
-    //fetch(PrimitiveSettings.URL.api + "posts?filter[taxonomy]=category&filter[term]=philosophy&page=" + this.state.page)
-    fetch("https://nice2b.me/wp-json/wp/v2/posts/?filter[taxonomy]=category&filter[term]=" + this.state.category + "&page=" + this.state.page)
-      .then(response => {
-        for (const pair of response.headers.entries()) {
-          // get total number of pages
-          if (pair[0] == "x-wp-totalpages") {
-            totalPages = pair[1];
-          }
-          if (this.state.page >= totalPages) {
-            this.setState({ getPostsInCat: false });
-          }
-        }
-        if (!response.ok) {
-          document.title = response.statusText + "| Nice2b.me";
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then(results => {
-        const allPostsInCat = this.state.posts.slice();
-        results.forEach(single => {
-          allPostsInCat.push(single);
-        });
-        this.setState({ posts: allPostsInCat });
-        document.title = "Category: " + this.state.category + " | Nice2b.me";
-      })
-      .catch(error => {
-        console.log(
-          "There was a problem with your fetch categories operation: " + error.message
-        );
-      });
-  }
+    return () => {
+      controller.destroy();
+    };
+  }, [pageNo]);
 
-  componentDidUpdate() {
-    // use ScrollMagic for infinite scrolling
+  useEffect(() => {
     const FadeInController = new ScrollMagic.Controller();
     document
       .querySelectorAll(".posts-container .col-md-4.card-outer")
@@ -100,23 +54,88 @@ class Categories extends React.Component {
         const FadeInScene = new ScrollMagic.Scene({
           triggerElement: item.children[0],
           reverse: false,
-          triggerHook: 1
+          triggerHook: 1,
         })
           .setClassToggle(item, "fade-in")
           .addTo(FadeInController);
       });
-  }
-  render() {
-    if (!this.state.posts.length === 0) {
-      return <img src={LoadingIcon} alt="Loading Categories" className="active" id="loader" />
-    }
+
+    return () => {
+      FadeInController.destroy();
+    };
+  }, [posts]);
+
+  const getMorePostsInCat = () => {
+    let url = window.location.href.split("/");
+    let slug = url.pop() || url.pop();
+    let totalPages;
+    let endpoint = PrimitiveSettings.URL.api + "posts/?filter[taxonomy]=category&filter[term]=" + slug + "&page=" + pageNo;
+
+    console.log(slug, category);
+
+    setCategory(slug);
+
+    fetch(endpoint)
+      .then((response) => {
+        for (const pair of response.headers.entries()) {
+          if (pair[0] === "x-wp-totalpages") {
+            totalPages = pair[1];
+            console.log("totalPages", totalPages);
+          }
+
+          if (pageNo >= totalPages) {
+            setGetPostsInCat(false);
+          }
+          else {
+            setPageNo(pageNo + 1);
+          }
+        }
+        if (!response.ok) {
+          document.title = `${response.statusText} | Nice2b.me`;
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((results) => {
+        setPosts((prevPosts) => [...prevPosts, ...results]);
+        document.title = `Category: ${category} | Nice2b.me`;
+      })
+      .catch((error) => {
+        console.log("There has been a problem with your fetch operation: " + error.message);
+      });
+  };
+
+  if (!posts.length) {
     return (
       <div className="container">
-        <h1 className="posts-title">Posts by Category: {this.state.category}</h1>
-        <PostList posts={this.state.posts} />
+        {loading ? (
+          <div className="row">
+            <div className="col text-center">
+              <Loader
+                height="100"
+                width="100"
+                wrapperClass="justify-content-center"
+              />
+              <p className="display-font fs-2 blink">I like blinking, I do...</p>
+            </div>
+          </div>
+        ) : (
+          <div className="row">
+            <div className="col text-center">
+              <p className="display-4">No posts found</p>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
-}
 
-export default withRouter(Categories);
+  return (
+    <div className="container">
+        <h1 className="text-center">{PrimitiveSettings.theme_posts_title} about {category}</h1>
+        <PostList posts={posts} />
+    </div>
+  );
+};
+
+export default Categories;
