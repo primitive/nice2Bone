fbb269d48a7c12669d0ba25b13a5b18f0cec6649
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-13 13:45:50 +0100

    rework

--- src/rocks/post-single.jsx
+++ src/rocks/post-single.jsx
@@ -1,91 +1,124 @@
 /**
  * The Post List Component
  * @package Nice2B One
+ * 2023
  */
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import Preloader from "../pebbles/loader";
 import Placeholder from "../n2b_placeholder1.jpg";
-import LoadingIcon from "../loading-icon.gif";
 
-const PostList = ({ posts }) => {
+const PostSingle = ({ post }) => {
   const renderPosts = () => {
 
-    //sk-dev: debug 
-    console.log(posts);
+    // sk-dev: debug 
+    console.log(post);
     
-    return posts.map((post, i) => {
       return (
-        <article className="col-md-4 card-outer" key={i}>
-          <div className="card">
+        <div className="col">
+        <article className="card fade-in">
+        <div className="img-outer"></div>
 
-              <Link to={PrimitiveSettings.path + "posts/" + post.slug + "/"}>
-                <img
-                  src={
-                    post.featured_image_src
-                      ? post.featured_image_src
-                      : Placeholder
-                  }
-                  className="card-img-top"
-                  alt={post.title.rendered}
-                  title={post.title.rendered}
-                />
-              </Link>
+        <div className="card-body">
+          <h1
+            className="card-title"
+            dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+          />
+          {/*
+          {this.state.post.featured_image_src ? (
+            <img
+              className="featured-image"
+              src={this.state.post.featured_image_src}
+              alt="featured image"
+            />
+          ) : null}
+*/}
 
-            <div className="card-body post-article post-details">
-
-              <h3 className="card-title">
-                <Link to={PrimitiveSettings.path + "posts/" + post.slug + "/"} dangerouslySetInnerHTML={{ __html: post.title.rendered }}></Link>
-              </h3>
-
-              <div className="card-meta">
-                <div className="entry-info">
-                  <i className="fas fa-folder-open"></i>
-                    {post.post_category.length ? post.post_category.map((item, index) => (
-                      <a key={item.toString()} href={PrimitiveSettings.path + 'category/' + post.post_category_slug[index] + "/"}>{item + " "}</a>)
-                    ) : ', '
-                    }
-                </div>
-              </div>
-
-              <p className="card-subtext">
-                <small className="text-muted">
-                  {post.author_name} &ndash; {post.published_date}
-                </small>
-              </p>
-              
-              <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
-
-              <div className="read-more">
-                <span className="button default">
-                  <Link className="button default" to={PrimitiveSettings.path + "posts/" + post.slug + "/"}>Read More</Link>
-                </span>
-              </div>
-
-            </div>
+          <p
+            className="card-text"
+            dangerouslySetInnerHTML={{
+              __html: post.content.rendered,
+            }}
+          />
+        </div>
+        <div className="card-meta">
+          <p className="card-text">
+            <small className="text-muted">
+              {post.author_name} &ndash; {post.published_date}
+            </small>
+          </p>
+          <div className="entry-info">
+            <span>
+              <i className="fas fa-folder-open"></i>
+              {post.post_category.length
+                ? post.post_category.map((item, index) => (
+                    <Link
+                      key={item.toString()}
+                      rel="category"
+                      to={
+                        PrimitiveSettings.path +
+                        "category/" +
+                        post.post_category_slug[index] +
+                        "/"
+                      }
+                    >
+                      {item + " "}
+                    </Link>
+                  ))
+                : ", "}
+            </span>{" "}
+            <span>
+              <i className="fas fa-tag"></i>
+              {post.post_tag.length
+                ? post.post_tag.map((item, index) => (
+                    <Link
+                      key={item.toString()}
+                      rel="tag"
+                      to={
+                        PrimitiveSettings.path +
+                        "tag/" +
+                        post.post_tag_slug[index] +
+                        "/"
+                      }
+                    >
+                      {item + " "}
+                    </Link>
+                  ))
+                : ", "}
+            </span>
           </div>
-        </article>
+        </div>
+      </article>
+      </div>
       );
-    });
+
   };
 
   const renderEmpty = () => {
-    return <img src={LoadingIcon} alt="Loading Posts" className="active" id="loader" />;
+    return (
+      <div className="row">
+        <div className="col text-center">
+          <Preloader />
+          <p className="display-font fs-2 blink">Loading</p>
+        </div>
+      </div>
+    )
   };
 
-  if (!posts) {
-    return null;
-  }
+  // if (!posts) {
+  //   return null;
+  // }
 
   return (
-    <div className="row posts-container">
-      {posts.length ? renderPosts() : renderEmpty()}
+    <div className="row post-container">
+      {post ? renderPosts() : renderEmpty()}
     </div>
   );
 };
 
-PostList.propTypes = {
-  posts: PropTypes.array.isRequired
+PostSingle.propTypes = {
+  post: PropTypes.object.isRequired
 };
 
-export default PostList;
+export default PostSingle;
