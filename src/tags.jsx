0b2ebf619537830c39335384a6e24d5720455dee
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-13 18:52:33 +0100

    wip

--- src/tags.jsx
+++ src/tags.jsx
@@ -4,55 +4,47 @@
  * 2023
  */
 import React, { useState, useEffect } from "react";
-import { Rings as Loader } from "react-loader-spinner";
+import Preloader from "../pebbles/loader";
 import PostList from "../rocks/post-list";
 import { handleBeforeUnload } from "../helpers";
 // import ReactGA from "react-ga";
-// import { useNavigate } from "react-router";
-
-// import ReactGA from 'react-ga';
-
 
 const Tags = (props) => {
+  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
   const [tag, setTag] = useState('');
-  const [page, setPage] = useState(0);
+  const [pageNo, setPageNo] = useState(1);
   const [getPostsWithTag, setGetPostsWithTag] = useState(true);
-  const [controller, setController] = useState(false);
 
   useEffect(() => {
-    window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
-    };
 
     // init ScrollMagic Controller
     const controller = new ScrollMagic.Controller();
-
-    // build scene
     const scene = new ScrollMagic.Scene({
       triggerElement: "#footer",
-      triggerHook: "onEnter"
+      triggerHook: "onEnter",
     })
       .addTo(controller)
-      .on("enter", e => {
-        if (getPostsWithTag) {
-          getMorePostsWithTag();
+      .on("enter", (e) => {
+        if (getPostsInCat) {
+          getMorePostsInCat();
         }
       });
-    // ReactGA.pageview(window.location.pathname + window.location.search);
+
+    document.title = PrimitiveSettings.theme_name + " - " + PrimitiveSettings.theme_posts_title;
     document.body.className = "";
-    document.body.classList.add('tag-list');
+    document.body.classList.add("tag-list");
+
+    //ReactGA.pageview(window.location.pathname + window.location.search);
+
+    window.onbeforeunload = handleBeforeUnload;
 
     return () => {
-      // clean up on unmount
-      window.onbeforeunload = null;
       controller.destroy();
-      document.body.classList.remove('tag-list');
     };
-  }, []);
+  }, [pageNo]);
 
   useEffect(() => {
-    // use ScrollMagic for infinite scrolling
     const FadeInController = new ScrollMagic.Controller();
     document
       .querySelectorAll(".posts-container .col-md-4.card-outer")
@@ -61,96 +53,7 @@ const Tags = (props) => {
         const FadeInScene = new ScrollMagic.Scene({
           triggerElement: item.children[0],
           reverse: false,
-          triggerHook: 1
-        })
-          .setClassToggle(item, "fade-in")
-          .addTo(FadeInController);
-      });
-  }, [posts]);
-
-  const getMorePostsWithTag = async () => {
-    let url = window.location.href.split("/");
-    let slug = url.pop() || url.pop();
-    let totalPages;
-    setPage(page + 1);
-    setTag(slug);
-
-    try {
-      const response = await fetch(`https://nice2b.me/wp-json/wp/v2/posts/?filter[tag]=${tag}&page=${page}`);
-      console.log("tags response", response);
-
-      for (const pair of response.headers.entries()) {
-        // getting the total number of pages
-        if (pair[0] == "x-wp-totalpages") {
-          totalPages = pair[1];
-        }
-
-        if (page >= totalPages) {
-          setGetPostsWithTag(false);
-        }
-      }
-
-      if (!response.ok) {
-        document.title = response.statusText + "| Nice2b.me";
-        throw Error(response.statusText);
-      }
-
-      const results = await response.json();
-      const allPosts = posts.slice();
-      results.forEach(single => {
-        allPosts.push(single);
-      });
-      setPosts(allPosts);
-    } catch (error) {
-      console.log(
-        "There has been a problem with your fetch operation: " + error.message
-      );
-    }
-  };
-
-  useEffect(() => {
-    if (getPostsWithTag) {
-      getMorePostsWithTag();
-    }
-  }, [getPostsWithTag]);
-
-  useEffect(() => {
-    // init ScrollMagic Controller
-    const controller = new ScrollMagic.Controller();
-
-    // build scene
-    const scene = new ScrollMagic.Scene({
-      triggerElement: "#footer",
-      triggerHook: "onEnter"
-    })
-      .addTo(controller)
-      .on("enter", e => {
-        if (getPostsWithTag) {
-          getMorePostsWithTag();
-        }
-      });
-
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    document.body.className = "";
-    document.body.classList.add('tag-list');
-
-    return () => {
-      controller.destroy();
-      document.body.classList.remove('tag-list');
-    }
-  }, []);
-
-  useEffect(() => {
-    // use ScrollMagic for infinite scrolling
-    const FadeInController = new ScrollMagic.Controller();
-    document
-      .querySelectorAll(".posts-container .col-md-4.card-outer")
-      .forEach(function (item) {
-        // build a scene
-        const FadeInScene = new ScrollMagic.Scene({
-          triggerElement: item.children[0],
-          reverse: false,
-          triggerHook: 1
+          triggerHook: 1,
         })
           .setClassToggle(item, "fade-in")
           .addTo(FadeInController);
@@ -158,25 +61,77 @@ const Tags = (props) => {
 
     return () => {
       FadeInController.destroy();
-    }
+    };
   }, [posts]);
 
-  if (!posts.length === 0) {
-    return <img src={LoadingIcon} alt="loading Tags" className="active" id="loader" />;
-  }
-  return (
+  //const getMorePostsWithTag = async () => {
+  const getMorePostsWithTag = () => {
+    let url = window.location.href.split("/");
+    let slug = url.pop() || url.pop();
+    let totalPages;
+    let endpoint = PrimitiveSettings.URL.api + "posts/?filter[taxonomy]=category&filter[tag]=" + slug + "&page=" + pageNo;
 
-    <div>
+    console.log(slug);
+
+    setTag(slug);
+
+    fetch(endpoint)
+      .then((response) => {
+        for (const pair of response.headers.entries()) {
+          if (pair[0] === "x-wp-totalpages") {
+            totalPages = pair[1];
+            console.log("totalPages", totalPages);
+          }
+
+          if (pageNo >= totalPages) {
+            setGetPostsWithTag(false);
+          }
+          else {
+            setPageNo(pageNo + 1);
+          }
+        }
+        if (!response.ok) {
+          document.title = `${response.statusText} | Nice2b.me`;
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((results) => {
+        setPosts((prevPosts) => [...prevPosts, ...results]);
+        document.title = `Tag: ${tag} | Nice2b.me`;
+      })
+      .catch((error) => {
+        console.log("There has been a problem with your fetch operation: " + error.message);
+      });
+  };
+
+  if (!posts.length) {
+    return (
       <div className="container">
-        <h1 className="posts-title">Posts with Tag: {tag}</h1>
+        {loading ? (
+          <div className="row">
+            <div className="col text-center">
+              <Preloader />
+              <p className="display-font fs-2 blink">I like blinking, I do...</p>
+            </div>
+          </div>
+        ) : (
+          <div className="row">
+            <div className="col text-center">
+              <p className="display-4">No posts found</p>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="container posts-container">
-        <div className="row">
-          <PostList posts={posts} />
-        </div>
-      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+        <h1 className="text-center">{PrimitiveSettings.theme_posts_title} about {category}</h1>
+        <PostList posts={posts} />
     </div>
   );
-}
+};
 
-export default Tags;
+export default Categories;
