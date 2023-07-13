9c42aeb997aecdbb32a0a2ff422414498a620dec
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-13 14:53:18 +0100

    review

--- src/not-found.jsx
+++ src/not-found.jsx
@@ -1,22 +1,31 @@
+/**
+ * The Not Found Component
+ * @package Nice2B One
+ * 2023
+ */
 import React from "react";
-import withRouter from './withrouter';
+// import withRouter from './withrouter';
 import { Link } from "react-router-dom";
 
 const NotFound = () => (
-  <div id="content">
-    <article className="container post-entry">
-      <div className="card">
-        <div className="card-body">
+  <div id="row">
+    <div className="col">
+      <article className="card fade-in fill-w">
+        <div className="card-body text-center pb-5">
           <h1 className="card-title">404 Page Not Found!</h1>
-          <p className="card-text">The page you requested does not exist.</p>
-          <p className="card-text button">
-            <Link to={PrimitiveSettings.path}>Return to homepage</Link>
+          <p className="card-text display-font fs-3 p-4">
+            The page you requested does not exist.
+          </p>
+          <p className="card-text justify-content-center">
+            <Link to={PrimitiveSettings.path} className="btn btn-primary">
+              Return to homepage
+            </Link>
           </p>
         </div>
-      </div>
-    </article>
+      </article>
+    </div>
   </div>
 );
 
-//export default NotFound;
-export default withRouter(NotFound)
+export default NotFound;
+// export default withRouter(NotFound)
