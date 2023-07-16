8ebf9f084cf42da96757df6a06b74980046cae4f
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-16 12:22:28 +0100

    update loader + head tags

--- public/index.php
+++ public/index.php
@@ -14,17 +14,10 @@
  <html <?php language_attributes(); ?> class="no-js">
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <!-- Mobile IE allows us to activate ClearType technology for smoothing fonts for easy reading -->
-        <meta http-equiv="cleartype" content="on">
-
-        <!-- Responsive and mobile friendly stuff -->
-        <meta name="HandheldFriendly" content="True">
-        <meta name="MobileOptimized" content="320">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
         <!-- Let WordPress set Initial Page Title -->
         <title>
             <?php
@@ -36,19 +29,55 @@
             bloginfo('name'); 
             ?>
         </title>
-        <!-- Always have wp_head() just before the closing </head>
-            * tag of your theme, or you will break many plugins, which
-            * generally use this hook to add elements to <head> such
-            * as styles, scripts, and meta tags.
-        -->
+
+        <!-- Always have wp_head() just before the closing </head> plugins use this hook to add stuff -->
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?> >
         <div id="root">
+            <style>
+                p {
+                    padding: 6vmin;
+                    color: lightslategray;
+                    font-size: 18px;
+                    font-weight: bold;
+                    font-style: italic;
+                    text-align: center;
+                    letter-spacing: 1px;
+                }
+                .loader {
+                    --color: darkslategray;
+                    --size-dot: 15px;
+                    display: block;
+                    position: relative;
+                    width: 50%;
+                    margin: calc(100vh/4) auto 0;
+                }
+                .loader::before,
+                .loader::after {
+                    content: '';
+                    box-sizing: border-box;
+                    position: absolute;
+                    width: var(--size-dot);
+                    height: var(--size-dot);
+                    background-color: var(--color);
+                    border-radius: 50%;
+                    animation: loading 1.2s ease-in-out infinite;
+                }
+                .loader::before { left: calc(50% - 1.6vmin - var(--size-dot)); }
+                .loader::after {
+                    left: calc(50% + 1.6vmin);
+                    animation-delay: -0.4s;
+                }
+                @keyframes loading {
+                    0%, 100% { transform: translateY(-3.6vmin); opacity: 1; }
+                    44% { transform: translateY(3.6vmin); opacity: 0.3; }
+                }
+
+            </style>
             <div id="content">
-                <div class="loader-gif">
-                    <img src="<?php echo home_url() ?>/wp-content/themes/nice2bone/dist/images/loading-icon.gif" alt="Brain is Loading...">
-                </div>
+                <div class="loader"></div>
+                <p>processing</p>
             </div>
         </div>
         <?php wp_footer(); ?>
