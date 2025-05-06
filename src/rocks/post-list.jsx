13fdebcd1a6a0822c6afaf9b928b2f43465366eb
Shaun Knowles <shaun@primitivedigital.uk>
2025-05-06 19:19:35 +0100

    add link component

--- src/rocks/post-list.jsx
+++ src/rocks/post-list.jsx
@@ -1,39 +1,39 @@
 /**
  * The Post List Component
- * @package Nice 2B
+ * @package Nice2B One
+ * 2025
  */
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Preloader from "../pebbles/loader";
+import PostLink from "../pebbles/post-link";
+import CatLink from "../pebbles/category-link";
 import Placeholder from "../n2b_placeholder1.jpg";
 
 const PostList = ({ posts }) => {
 
-  // const PostLink = ({ slug, children }) => (
-  //   <Link to={`${PrimitiveSettings.path}posts/${slug}/`}>{children}</Link>
-  // );
 
   const renderPosts = () => {
     return posts.map((post, i) => {
       return (
         <article className="col-md-4 card-outer" key={post.id || post.slug}>
           <div className="card">
-            <Link to={PrimitiveSettings.path + "posts/" + post.slug + "/"}>
+            <PostLink slug={post.slug}>
               <img
                 src={post.featured_image_src || Placeholder}
                 className="card-img-top"
                 alt={post.title.rendered}
                 title={post.title.rendered}
               />
-            </Link>
+            </PostLink>
 
             <div className="card-body post-article post-details">
               <h2 className="card-title">
-                <Link
-                  to={PrimitiveSettings.path + "posts/" + post.slug + "/"}
+                <PostLink
+                  slug={post.slug}
                   dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-                ></Link>
+                ></PostLink>
               </h2>
 
               <div className="card-meta">
@@ -53,17 +53,13 @@ const PostList = ({ posts }) => {
                   <i className="fas fa-cat" title="cat-egories"></i>
                   {post.post_category.length
                     ? post.post_category.map((item, index) => (
-                        <a
+                        <CatLink
                           key={item.toString()}
-                          href={
-                            PrimitiveSettings.path +
-                            "category/" +
-                            post.post_category_slug[index] +
-                            "/"
-                          }
+                          slug={`${post.post_category_slug[index]}`}
                         >
-                          {item + " "}
-                        </a>
+                          {item}
+                          {index < post.post_category.length - 1 && ", "}
+                        </CatLink>
                       ))
                     : ", "}
                 </p>
@@ -72,13 +68,13 @@ const PostList = ({ posts }) => {
               <div className="card-excerpt" dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
 
               <div className="buttons">
-                <Link
+                <PostLink
                   className="btn btn-sketch"
-                  to={PrimitiveSettings.path + "posts/" + post.slug + "/"}
-                  alt={"Read post: "+post.title.rendered}
+                  to={`/posts/${post.slug}/`}
+                  alt={`Read post: ${post.title.rendered}`}
                 >
                   Read More
-                </Link>
+                </PostLink>
               </div>
             </div>
           </div>
