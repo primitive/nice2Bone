bed9816219e1f50977bde825b3c196a4485ac9fc
Shaun Knowles <shaun@primitivedigital.uk>
2025-05-07 13:50:58 +0100

    correct class in jsx

--- src/footer.jsx
+++ src/footer.jsx
@@ -17,16 +17,16 @@ const Footer = () => (
             
             <br/>
 
-            Website crafted by the digital alchemy of <a href="" class="display-font">primitiveshaun.</a><br/>
+            Website crafted by the digital alchemy of <a href="" className="display-font">primitiveshaun.</a><br/>
 
             Words and thoughts, welcomed or otherwise, provided by the mysterious mind of the magical Mr. K.<br/>
 
-            Beautiful stuff designed by <a href="" class="display-font">Scibble &amp; Sketch</a>.
+            Beautiful stuff designed by <a href="" className="display-font">Scibble &amp; Sketch</a>.
           </p>
           <p className="copyright">
             Copyright &copy; {PrimitiveSettings.title}{" "}
             {new Date().getFullYear()} - Nice 2B One by{" "}
-            <a href="https://primitive.industries" class="display-font"> Primitive Industries</a>
+            <a href="https://primitive.industries" className="display-font"> Primitive Industries</a>
           </p>
         </div>
       </div>
