32355f0b1a0d9d2ecf9de80fa26b3e7e5acb40f4
Shaun Knowles <shaun@primitiveweb.dev>
2025-08-04 18:35:03 +0100

    update not found

--- src/not-found.jsx
+++ src/not-found.jsx
@@ -1,30 +1,32 @@
 /**
  * The Not Found Component
  * @package Nice2B One
- * 2023
+ * 2025
  */
 import React from "react";
-// import withRouter from './withrouter';
 import { Link } from "react-router-dom";
 
 const NotFound = () => (
-  <div id="row">
-    <div className="col">
+  <div className="container post-entry">
+
+
       <article className="card fade-in fill-w">
         <div className="card-body text-center pb-5">
           <h1 className="card-title">404 Page Not Found!</h1>
           <p className="card-text display-font fs-3 p-4">
             The page you requested does not exist.
           </p>
-          <p className="card-text justify-content-center">
-            <Link to={PrimitiveSettings.path} className="btn btn-primary">
+          <p className="card-text">
+
+            <Link to={PrimitiveSettings.path} className="btn btn-primary btn-lg">
               Return to homepage
             </Link>
+
           </p>
         </div>
       </article>
     </div>
-  </div>
+
 );
 
 export default NotFound;
