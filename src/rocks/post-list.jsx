9cb579314f2097e87247630c4f7bd5c545dbffce
Shaun Knowles <shaun@primitiveweb.dev>
2025-08-07 17:59:57 +0100

    2025 update

--- src/rocks/post-list.jsx
+++ src/rocks/post-list.jsx
@@ -4,99 +4,26 @@
  * 2025
  */
 import React from "react";
-//import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Preloader from "../pebbles/loader";
-import PostLink from "../pebbles/post-link";
-import CatLink from "../pebbles/category-link";
-import Placeholder from "../n2b_placeholder1.jpg";
-
+import PostCard from "../rocks/post-card";
 
 const PostList = ({ posts }) => {
 
-  const renderPosts = () => {
-    return posts.map((post, i) => {
-      return (
-        <article className="col-md-4 card-outer" key={post.id || post.slug}>
-          <div className="card">
-            <PostLink slug={post.slug}>
-              <img
-                src={post.featured_image_src || Placeholder}
-                className="card-img-top"
-                alt={post.title.rendered}
-                title={post.title.rendered}
-              />
-            </PostLink>
-
-            <div className="card-body post-article post-details">
-              <h2 className="card-title">
-                <PostLink
-                  slug={post.slug}
-                  dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-                ></PostLink>
-              </h2>
-
-              <div className="card-meta">
-
-                <p className="post-meta text-muted d-flex">
-                  <span className="card-author">
-                    <i className="fas fa-pen-fancy" title="penned by"></i>
-                    {post.author_name}
-                  </span>
-                  <span className="card-published">
-                    <i className="far fa-calendar-alt" title="dated"></i>
-                    {post.published_date}
-                  </span>
-                </p>
-
-                <p className="post-tax">
-                  <i className="fas fa-cat" title="cat-egories"></i>
-                  {post.post_category?.length
-                    ? post.post_category.map((item, index) => (
-                        <CatLink
-                          key={item.toString()}
-                          slug={`${post.post_category_slug[index]}`}
-                        >
-                          {item}
-                          {index < post.post_category.length - 1 && ", "}
-                        </CatLink>
-                      ))
-                    : ", "}
-                </p>
-              </div>
-
-              <div className="card-excerpt" dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
-
-              <div className="buttons">
-                <PostLink
-                  slug={post.slug}
-                  className="btn btn-sketch"
-                  alt={`Read post: ${post.title.rendered}`}
-                >
-                  Read More
-                </PostLink>
-              </div>
-            </div>
-          </div>
-        </article>
-      );
-    });
-  };
-
-  const renderEmpty = () => {
-    return (
-      <div className="row">
+  console.log(posts);
+  
+  return (
+    <div className="row posts-container">
+      {posts.length ? (
+        posts.map((post, i) => (
+          <PostCard key={post.id || post.slug} post={post} index={i} />
+        ))
+      ) : (
         <div className="col text-center">
           <Preloader />
           <p className="display-font fs-2 blink">Loading</p>
         </div>
-      </div>
-    );
-  };
-
-  return (
-    <div className="row posts-container">
-      {posts.length ? renderPosts() : renderEmpty()}
+      )}
     </div>
   );
 };
