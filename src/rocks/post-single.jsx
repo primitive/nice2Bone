c3d1ed6e3916d3643b469578875cf197f5b41536
Shaun Knowles <shaun@primitiveweb.dev>
2025-09-25 02:19:48 +0100

    optimise

--- src/rocks/post-single.jsx
+++ src/rocks/post-single.jsx
@@ -9,7 +9,7 @@ import PostLink from "../pebbles/post-link";
 import CatLink from "../pebbles/category-link";
 import TagLink from "../pebbles/tag-link";
 import Image from "../pebbles/image";
-import Placeholder from "../n2b_placeholder1.jpg";
+import Placeholder from "../n2b_placeholder1.webp";
 import { cleanText } from "../helpers";
 
 
